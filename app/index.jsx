f83b583f53ce3f5a69b455cb6e4058fc87e36556
mranuj123 <dotsyko@gmail.com>
2024-11-26 13:14:14 +0545

    dark and light mode added

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,27 @@
 import { data } from "@/data/todo";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { Pressable, Text, TextInput, View,StyleSheet, FlatList } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
-
+import {Inter_500Medium, useFonts} from "@expo-google-fonts/inter"
+import { ThemeContext } from "@/context/ThemeContext";
+import Octicons from '@expo/vector-icons/Octicons';
 
 export default function Index() {
   const [todos, setTodos] = useState(data.sort((a, b) => b.id - a.id));
   const [text,setText] = useState("");
+  const {theme,colorScheme,setColorScheme} = useContext(ThemeContext);
 
+
+  const [loaded,error] = useFonts({
+    Inter_500Medium
+  })
+
+  if(!loaded && !error){
+    return null;
+  }
+  const styles = createStyles(theme,colorScheme);
   const addTodo = () => {
       if(text.trim()){
         const newId = todos.length > 0 ? todos[0].id + 1 : 1;
@@ -58,6 +70,16 @@ export default function Index() {
             <Pressable onPress={addTodo} style={styles.addButton}> 
               <Text style={styles.addButtonText}>Add</Text>
             </Pressable>
+            <Pressable 
+            onPress={() => setColorScheme(colorScheme === 'light' ? 'dark' : 'light')}
+            style={{marginLeft:10}}
+            >
+              {
+                colorScheme === 'dark' ?
+                <Octicons name = "moon" size={36} color={theme.text} selectable={undefined} style={{width:36}}/> 
+                : <Octicons name = "sun" size={36} color={theme.text} selectable={undefined} style={{width:36}} />
+              }
+            </Pressable>
         </View>
         <FlatList
         data={todos}
@@ -69,10 +91,13 @@ export default function Index() {
   );
 }
 
-const styles= StyleSheet.create({
+function createStyles(theme,colorScheme)
+{ 
+
+return StyleSheet.create({
   container:{
     flex:1,
-    backgroundColor:"black"
+    backgroundColor:theme.background
   },
   inputContainer:{
     flexDirection:"row",
@@ -91,18 +116,19 @@ const styles= StyleSheet.create({
    borderRadius:5,
    padding:5,
    marginRight:10,
-   color:"white",
+   color:theme.text,
    fontSize:18,
+   fontFamily:"Inter_500Medium",
    minWidth:0
   },
   addButton:{
-    backgroundColor:"white",
+    backgroundColor:theme.button,
     padding:10,
     borderRadius:5
   },
   addButtonText:{
     fontSize:18,
-    color:"black"
+    color:colorScheme === 'dark' ? 'black' : 'white',
   }
   ,
   todoItem:{
@@ -121,8 +147,9 @@ const styles= StyleSheet.create({
   },
   todoText:{
     flex:1,
-    color:"white",
-    fontSize:18
+    color:theme.text,
+    fontSize:18,
+    fontFamily:"Inter_500Medium",
   },
   completedText:{
     textDecorationLine:"line-through",
@@ -130,3 +157,4 @@ const styles= StyleSheet.create({
   }
  
 })
+}
